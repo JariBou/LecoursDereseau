9ecfa6d6df3c44fd293b825d00d37728cf8ef07c
Brach <alessandrotano@outlook.fr>
2026-02-17 12:08:54 +0100

    [MOD] Test multi

--- Assets/_project/Scripts/GameLogic/MainMenuHandler.cs
+++ Assets/_project/Scripts/GameLogic/MainMenuHandler.cs
@@ -5,7 +5,7 @@ public class MainMenuHandler : MonoBehaviour
 {
     public void ButtonHostGame()
     {
-        SceneManager.LoadScene("PipotamPremiumServer", LoadSceneMode.Additive);
+        SceneManager.LoadScene("test-tome-sv", LoadSceneMode.Additive);
     }
 
     public void ButtonJoinGame()
@@ -14,6 +14,7 @@ public class MainMenuHandler : MonoBehaviour
         // Check val in input
         // Start Client server and try to connect to server
         // Load scene
+        SceneManager.LoadScene("test-tome-cl", LoadSceneMode.Additive);
     }
 
 
